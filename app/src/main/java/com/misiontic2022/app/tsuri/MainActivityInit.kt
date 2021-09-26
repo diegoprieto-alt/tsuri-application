a322488e3882070b83b9ed8b91393c47b54932ce
diegoprieto-alt <diegoprieto.dl@gmail.com>
2021-09-26 06:52:50 -0500

    added intent to connect activities

--- app/src/main/java/com/misiontic2022/app/tsuri/MainActivityInit.kt
+++ app/src/main/java/com/misiontic2022/app/tsuri/MainActivityInit.kt
@@ -1,11 +1,20 @@
 package com.misiontic2022.app.tsuri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivityInit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_init)
     }
+
+    fun sendMessage(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
