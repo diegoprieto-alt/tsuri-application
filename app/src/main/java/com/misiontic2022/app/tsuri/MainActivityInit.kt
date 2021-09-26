105bc7e92daf68f2588ef44510088b996ba5c51c
diegoprieto-alt <diegoprieto.dl@gmail.com>
2021-09-26 06:41:37 -0500

    Added Diego's project

---
+++ app/src/main/java/com/misiontic2022/app/tsuri/MainActivityInit.kt
@@ -0,0 +1,11 @@
+package com.misiontic2022.app.tsuri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityInit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_init)
+    }
+}
