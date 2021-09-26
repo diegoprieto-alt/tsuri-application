095920cc06da0910f841426d2cedecbec7108c71
diegoprieto-alt <diegoprieto.dl@gmail.com>
2021-09-26 08:35:51 -0500

    added fragments to move between activities

--- app/src/main/java/com/misiontic2022/app/tsuri/SelectorFragmentActivity.kt
+++ app/src/main/java/com/misiontic2022/app/tsuri/SelectorFragmentActivity.kt
@@ -4,17 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 
-class MainActivityInit : AppCompatActivity() {
+class SelectorFragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_init)
+        setContentView(R.layout.activity_selector_fragment)
     }
 
-    fun clickMore(view: View){
-        val intent = Intent(this, SelectorFragmentActivity::class.java)
+    fun clickPoi(view: View){
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
-
 }
